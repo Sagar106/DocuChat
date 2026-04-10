14004ba2b056f2fa567616307e94c308693a288d
Sagar Upadhyay <sagarupadhyay106@gmail.com>
2026-04-10 23:57:50 +0530

    implementing register and login controllers

--- docu-chat-server/src/app.js
+++ docu-chat-server/src/app.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-import passport from "passport";
+import authRoutes from "./routes/auth.routes.js";
 
 dotenv.config();
 
@@ -11,5 +11,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
+
+app.use("/api/auth", authRoutes);
 
 export default app;
